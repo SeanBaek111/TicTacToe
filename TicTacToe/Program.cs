b862ea3d9ef48c572d35bbd1f28abe7d65dbdb1c
Benjamin 'PenguinJoints' Wang <p3nj@tutanota.com>
2023-03-15 20:40:31 +1000

    FIXME: change some code structure

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using static System.Console;
 
-namespace TicTacToe
+namespace TicTacToe;
+
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        public static void Main()
+        // Check if the save file exists.
+        bool saveFile = File.Exists("save.cfg");
+
+        Menu menu1 = new Menu();
+        // Declare the user decision variable
+        int nSelection;
+
+        // If Save file exists, do the confirmation.
+        //
+        if (saveFile)
         {
-            // Check if the save file exists.
-            bool saveFile = File.Exists("save.cfg");
-            // Declare the user decision variable
-            int nSelection;
-
-            Menu menu1 = new Menu();
-
-            // If Save file exists, do the confirmation.
-            //
-            if (saveFile)
+            while (true)
             {
                 menu1.SetQuestion("Load Last Save Game?");
                 menu1.AddMenu("Yes");
@@ -40,9 +42,12 @@ namespace TicTacToe
                         if (confirmWipeAnswer == 1)
                         {
                             // Go to new Game.
+                            NewGame();
                         }
-                        else if (confirmWipeAnswer == 2) {
+                        else if (confirmWipeAnswer == 2)
+                        {
                             // back to first section. 
+                            continue;
                         }
                         break;
                     case 3:
@@ -50,109 +55,121 @@ namespace TicTacToe
                         break;
                 }
             }
-            else
-            {
-                Game game = null;
-                Player[] players = new Player[2];
-
-
-                Menu menu2 = new Menu();
-                menu2.SetQuestion("Welcome to TTT\nSelect an Option");
-                menu2.AddMenu("Wild Tic Tac Toe");
-                menu2.AddMenu("Numerical Tic Tac Toe");
-                menu2.AddMenu("Load Last Save Game?");
-                menu2.AddMenu("Help");
-                menu2.AddMenu("Quit");
-                nSelection = menu2.GetUserAnswer();
-
-
-                Menu menu3 = new Menu();
-                if (nSelection == 1)
-                {
-                    game = new WildTTT();
-
-                    menu3.SetQuestion("Wild Tic Tac Toe\nWho is playing?");
-                    menu3.AddMenu("Player vs Player");
-                    menu3.AddMenu("Player vs Computer");
-                    menu3.AddMenu("Back");
-                    nSelection = menu3.GetUserAnswer();
-
-                    if (nSelection == 1)
-                    {
-                        players[0] = new HumanPlayer("Player 1");
-                        players[1] = new HumanPlayer("Player 2");
-
-                        players[0].SetTurn(true);
-                        players[1].SetTurn(false);
-                    }
-                    else if (nSelection == 2)
-                    {
-                        players[0] = new HumanPlayer();
-                        players[1] = new ComputerPlayer();
-
-                        players[0].SetTurn(true);
-                        players[1].SetTurn(false);
-                    }
-
-                    game.SetPlayers(players);
-                    game.SetBoard(new TicTacToeBoard());
-                    game.Play();
-                }
-                else if (nSelection == 2)
-                {
-                    game = new NumericTTT();
-
-                    menu3.SetQuestion("Numerical Tic Tac Toe\nWho is playing?");
-                    menu3.AddMenu("Player vs Player");
-                    menu3.AddMenu("Player vs Computer");
-                    menu3.AddMenu("Back");
-                    nSelection = menu3.GetUserAnswer();
-
-                    if (nSelection == 1)
-                    {
-                        players[0] = new HumanPlayer("Player 1");
-                        players[1] = new HumanPlayer("Player 2");
-
-                        players[0].SetTurn(true);
-                        players[1].SetTurn(false);
-                    }
-                    else if (nSelection == 2)
-                    {
-                        players[0] = new HumanPlayer();
-                        players[1] = new ComputerPlayer();
-
-                        players[0].SetTurn(true);
-                        players[1].SetTurn(false);
-                    }
-
-                    game.SetPlayers(players);
-                    game.SetBoard(new TicTacToeBoard());
-                    game.Play();
-                }
-                else if (nSelection == 3)
-                {
-                    menu3.SetQuestion("Save Game\nDo you want to load your last saved game?");
-                    menu3.AddMenu("Load");
-                    menu3.AddMenu("Delete");
-                    menu3.AddMenu("Back");
-                    nSelection = menu3.GetUserAnswer();
-                }
-                else if (nSelection == 4)
-                {
-                    menu3.SetQuestion("SaveHelp Message");
-                    menu3.AddMenu("Wild Numerical TTT help");
-                    menu3.AddMenu("Numerical TTT Help");
-                    menu3.AddMenu("Back");
-                    nSelection = menu3.GetUserAnswer();
-                }
-
-
-            }
         }
-
-        static void LoadGame()
+        else
         {
-            WriteLine("Load Game");
+            NewGame();
         }
     }
+
+    static void NewGame()
+    {
+        // Declare the user decision variable
+        int nSelection;
+
+        // TODO: DO new game.
+        //
+        throw new NotImplementedException();
+
+        Game game = null;
+        Player[] players = new Player[2];
+
+
+        Menu menu2 = new Menu();
+        menu2.SetQuestion("Welcome to TTT\nSelect an Option");
+        menu2.AddMenu("Wild Tic Tac Toe");
+        menu2.AddMenu("Numerical Tic Tac Toe");
+        menu2.AddMenu("Load Last Save Game?");
+        menu2.AddMenu("Help");
+        menu2.AddMenu("Quit");
+        nSelection = menu2.GetUserAnswer();
+
+
+        Menu menu3 = new Menu();
+        if (nSelection == 1)
+        {
+            game = new WildTTT();
+
+            menu3.SetQuestion("Wild Tic Tac Toe\nWho is playing?");
+            menu3.AddMenu("Player vs Player");
+            menu3.AddMenu("Player vs Computer");
+            menu3.AddMenu("Back");
+            nSelection = menu3.GetUserAnswer();
+
+            if (nSelection == 1)
+            {
+                players[0] = new HumanPlayer("Player 1");
+                players[1] = new HumanPlayer("Player 2");
+
+                players[0].SetTurn(true);
+                players[1].SetTurn(false);
+            }
+            else if (nSelection == 2)
+            {
+                players[0] = new HumanPlayer();
+                players[1] = new ComputerPlayer();
+
+                players[0].SetTurn(true);
+                players[1].SetTurn(false);
+            }
+
+            game.SetPlayers(players);
+            game.SetBoard(new TicTacToeBoard());
+            game.Play();
+        }
+        else if (nSelection == 2)
+        {
+            game = new NumericTTT();
+
+            menu3.SetQuestion("Numerical Tic Tac Toe\nWho is playing?");
+            menu3.AddMenu("Player vs Player");
+            menu3.AddMenu("Player vs Computer");
+            menu3.AddMenu("Back");
+            nSelection = menu3.GetUserAnswer();
+
+            if (nSelection == 1)
+            {
+                players[0] = new HumanPlayer("Player 1");
+                players[1] = new HumanPlayer("Player 2");
+
+                players[0].SetTurn(true);
+                players[1].SetTurn(false);
+            }
+            else if (nSelection == 2)
+            {
+                players[0] = new HumanPlayer();
+                players[1] = new ComputerPlayer();
+
+                players[0].SetTurn(true);
+                players[1].SetTurn(false);
+            }
+
+            game.SetPlayers(players);
+            game.SetBoard(new TicTacToeBoard());
+            game.Play();
+        }
+        else if (nSelection == 3)
+        {
+            menu3.SetQuestion("Save Game\nDo you want to load your last saved game?");
+            menu3.AddMenu("Load");
+            menu3.AddMenu("Delete");
+            menu3.AddMenu("Back");
+            nSelection = menu3.GetUserAnswer();
+        }
+        else if (nSelection == 4)
+        {
+            menu3.SetQuestion("SaveHelp Message");
+            menu3.AddMenu("Wild Numerical TTT help");
+            menu3.AddMenu("Numerical TTT Help");
+            menu3.AddMenu("Back");
+            nSelection = menu3.GetUserAnswer();
+        }
+    }
+    static void LoadGame()
+    {
+        // TODO: Do load save file
+        WriteLine("Load Game");
+        throw new NotImplementedException();
+    }
 }
+
